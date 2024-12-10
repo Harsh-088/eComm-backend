890013f953e4d2ae11d29e7fc006a402548f97e4
harsh-scrobits <harshkumar.scrobits@gmail.com>
2024-12-10 13:31:08 +0530

    fixed auth bug

--- src/v1/router.ts
+++ src/v1/router.ts
@@ -7,5 +7,5 @@ import { userRouter } from "./user/user.router"
 export const v1Router = express.Router()
 
 v1Router.use("/v1", productRouter)
-v1Router.use("/v1", Auth.authenticated, cartRouter)
 v1Router.use("/v1", userRouter)
+v1Router.use("/v1", Auth.authenticated, cartRouter)
